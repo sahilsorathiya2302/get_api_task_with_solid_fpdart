4168bc27fa998aff43a7f0d421b0dc0746ab4a30
SahilKumar Sorathiya <130341054+Sahil231102@users.noreply.github.com>
2025-03-03 10:12:41 +0530

    commit

--- lib/feature/home/domain/use_cases/get_posts_use_case.dart
+++ lib/feature/home/domain/use_cases/get_posts_use_case.dart
@@ -4,6 +4,8 @@ import 'package:simple_get_api_task_wc/core/usecase/use_case.dart';
 import 'package:simple_get_api_task_wc/feature/home/domain/entities/post.dart';
 import 'package:simple_get_api_task_wc/feature/home/domain/repositories/post_repositories.dart';
 
+
+///PostRepositoryImpl not used direct
 class GetPostsUseCase implements UseCase<List<Post>, NoParams> {
   final PostRepository repository;
 
